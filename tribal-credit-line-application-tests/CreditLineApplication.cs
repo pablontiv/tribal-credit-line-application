0c089cc6326ee46198f4ee1904ec89670049b37e
Pablo Ontiveros <pablo.ontiveros@gmail.com>
2022-03-20 12:55:45 -0600

    FIX test solution with NUnit to manage DI

---
+++ tribal-credit-line-application-tests/CreditLineApplication.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tribal_credit_line_application.Util;
+
+namespace tribal.credit.line.application.tests
+{
+    internal class CreditLineApplication: WebApplicationFactory<Program>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddDependencies();
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}

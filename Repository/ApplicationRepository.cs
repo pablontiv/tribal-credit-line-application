11f8d3863ed16f19aef98660d1bdd536a3ce1733
Pablo Ontiveros <pablo.ontiveros@gmail.com>
2022-03-19 18:25:28 -0600

    ADD ApplicationRepository

---
+++ Repository/ApplicationRepository.cs
@@ -0,0 +1,25 @@
+﻿using tribal_credit_line_application.Model;
+
+namespace tribal_credit_line_application.Repository
+{
+    public class ApplicationRepository
+    {
+        private readonly Dictionary<int, ApplicationResult> applications = new Dictionary<int, ApplicationResult>();
+
+        public ApplicationResult? GetById(int id)
+        {
+            if (!applications.ContainsKey(id))
+                return null;
+
+            return applications[id];
+        }
+
+        public void Add(int id, ApplicationResult result)
+        {
+            if (applications.ContainsKey(id))
+                return;
+
+            applications.Add(id, result);
+        }
+    }
+}

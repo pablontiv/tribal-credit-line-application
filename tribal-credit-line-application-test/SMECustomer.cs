bf0a701cfe14ae25095d2ab7f860ee142b5620ab
Pablo Ontiveros <pablo.ontiveros@gmail.com>
2022-03-20 01:29:13 -0600

    FIX Directories

---
+++ tribal-credit-line-application-test/SMECustomer.cs
@@ -0,0 +1,16 @@
+using tribal_credit_line_application.Services;
+using Xunit;
+
+namespace tribal_credit_line_application_test
+{
+    public class SMECustomer
+    {
+        [Fact]
+        public void AcceptedCredit()
+        {
+            //var applicationService = new ApplicationService();
+
+
+        }
+    }
+}
